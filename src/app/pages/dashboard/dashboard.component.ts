458ca167630768cdc2f2d563e444a546c059f4b5
Eleazarsimba <eleazarsimba5@gmail.com>
2024-08-03 21:24:43 +0300

    Adding dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+interface DashboardItems {
+  title: string;
+  item_val: number;
+  scale: any;
+}
+
+interface Sms {
+  date: string;
+  time: string;
+  contact: string;
+  body: string;
+  status: string;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,4 +21,20 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  alldashitems: DashboardItems[] = [
+    { title: 'All Customers', item_val: 160, scale: 150 },
+    { title: 'Recent Chats', item_val: 150, scale: 140 },
+    { title: 'Recent Sales', item_val: 200, scale: 180 },
+    { title: 'Farmer Goals', item_val: 130, scale: 120 },
+    { title: 'Recent Adverts', item_val: 40, scale: 30 },
+    { title: 'Partners', item_val: 30, scale: 20 },
+    { title: 'Recent Payables', item_val: 10, scale: 20 },
+    { title: 'Sale Requests', item_val: 10, scale: 20 }
+  ];
+
+  messages: Sms[] = [
+    { date:'2024-08-03', time: '10:30:00', contact: 'All Customers', body: "This is a test date", status: "Resolved" },
+    { date:'2024-08-03', time: '11:00:00', contact: 'Recent Chats', body: "This is a test date", status: "Resolved" },
+    { date:'2024-08-03', time: '11:30:00', contact: 'Recent Sales', body: "This is a test date", status: "Resolved" }
+  ];
 }
