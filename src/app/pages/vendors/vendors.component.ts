094974a993e77d7dce49576e417409023daf310f
Eleazarsimba <eleazarsimba5@gmail.com>
2024-08-10 00:01:03 +0300

    Adding delete btn in home

--- src/app/pages/vendors/vendors.component.ts
+++ src/app/pages/vendors/vendors.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./vendors.component.scss']
 })
 export class VendorsComponent {
+  isModalOpen = false;
+  isDeleteModalOpen = false;
+  deleteItem:string = '';
 
+  toggleDeleteModal(id: any) {
+    this.deleteItem = id;
+    this.isDeleteModalOpen = !this.isDeleteModalOpen;
+  }
+
+  deleteItems(confirm: boolean){
+    if(confirm){
+      console.log('You have deleted '+this.deleteItem)
+      this.toggleDeleteModal(confirm)
+    }else{
+      this.toggleDeleteModal(confirm)
+      console.log('Canceled deletion of '+this.deleteItem)
+    }
+  }
 }
